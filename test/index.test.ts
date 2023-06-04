31fd15a42231a6fb41d5dd4671c3a1cba83378ee
Hugh McKenna <hfmckenna@outlook.com>
2023-06-04 16:28:36 +0100

    chore: Add front end helpers

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import { test } from 'node:test';
 import assert from 'node:assert';
-import { firstFn } from '../src';
+import { firstFn } from '../src/firstFn';
 
 test(firstFn.name, () => {
   assert.strictEqual(firstFn(), undefined);
